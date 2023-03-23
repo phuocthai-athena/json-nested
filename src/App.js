6d95fc9b33f13780bd5c76f2f4f74fcb8daadd5d
Thai <thaibui@everfit.io>
2023-03-23 22:31:44 +0700

    feat(master): 70%

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./Data/data.json";
+import "./App.css";
+
+function renderList(data) {
+  return Object.entries(data).map(([key, value]) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    let displayValue = null;
+
+    if (typeof value === "object" && value !== null) {
+      // Recursively render nested objects
+      const button = isExpanded ? (
+        <button className="button" onClick={() => setIsExpanded(false)}>
+          -
+        </button>
+      ) : (
+        <button className="button" onClick={() => setIsExpanded(true)}>
+          +
+        </button>
+      );
+
+      displayValue = (
+        <div className="nested-container">
+          {button}
+          <ul className={isExpanded ? "nested" : "nested hidden"}>
+            {renderList(value)}
+          </ul>
+        </div>
+      );
+    } else {
+      // Render primitive values as is
+      displayValue = value !== null ? value.toString() : "null";
+    }
+
+    return (
+      <li key={key}>
+        <div className="key-value-container">
+          <span className="key">{key}:</span>{" "}
+          <span className="value">{displayValue}</span>
+        </div>
+      </li>
+    );
+  });
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app-content">
+        <ul className="root">{renderList(data[0])}</ul>
+      </div>
     </div>
   );
 }
